597a246961f8305544d1e9b9d1c866cd6ef021c8
Dan Carlyon <dannycar@hotmail.co.uk>
2016-03-22 18:55:14 +0000

    Added a candy floss item

--- src/main/java/com/dancarlyon/carnival/item/ItemCandyFloss.java
+++ src/main/java/com/dancarlyon/carnival/item/ItemCandyFloss.java
@@ -3,9 +3,10 @@ package com.dancarlyon.carnival.item;
 import com.dancarlyon.carnival.handler.ConfigurationHandler;
 import net.minecraft.potion.Potion;
 
-public class ItemCandyFloss extends FoodItemCarnival {
-
-    public ItemCandyFloss() {
+public class ItemCandyFloss extends FoodItemCarnival
+{
+    public ItemCandyFloss()
+    {
         super(12, 0.9F, false);
         this.setUnlocalizedName("candy_floss");
     }
