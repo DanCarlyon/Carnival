f17ebcd67240663f7cb9ed2997210ab465fce178
Dan Carlyon <dannycar@hotmail.co.uk>
2016-05-02 09:54:56 +0100

    Added music disc icon and multi colored snowcone asset

--- src/main/java/com/dancarlyon/carnival/item/ItemCarnivalMusic.java
+++ src/main/java/com/dancarlyon/carnival/item/ItemCarnivalMusic.java
@@ -1,10 +1,13 @@
 package com.dancarlyon.carnival.item;
 
 
+import com.dancarlyon.carnival.creativetab.CreativeTabCarnival;
 import com.dancarlyon.carnival.reference.Reference;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.ItemRecord;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 
@@ -17,6 +20,7 @@ public class ItemCarnivalMusic extends ItemRecord {
         this.setRecordName = "carnival_music";
         this.maxStackSize = 1;
         this.setUnlocalizedName("carnival_music");
+        this.setCreativeTab(CreativeTabCarnival.CARNIVAL_TAB);
     }
 
     @SideOnly(Side.CLIENT)
@@ -30,10 +34,34 @@ public class ItemCarnivalMusic extends ItemRecord {
         return new ResourceLocation(Reference.MOD_ID + ":carnival_music");
     }
 
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack)
+    {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+
     @SideOnly(Side.CLIENT)
     public String getRecordNameLocal()
     {
         return StatCollector.translateToLocal("item.record." + this.recordName + ".desc");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister)
+    {
+        itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") +1));
     }
 
 }
--- src/main/java/com/dancarlyon/carnival/item/ItemCarnivalTicket.java
+++ src/main/java/com/dancarlyon/carnival/item/ItemCarnivalTicket.java
@@ -1,8 +1,20 @@
 package com.dancarlyon.carnival.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.World;
+
 public class ItemCarnivalTicket extends ItemCarnival {
     public ItemCarnivalTicket() {
         super();
         this.setUnlocalizedName("carnival_ticket");
+    }
+
+
+    public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer player)
+    {
+        player.addChatComponentMessage(new ChatComponentText(player.getDisplayName() + " Wonder what i can use this for?"));
+        return p_77659_1_;
     }
 }
