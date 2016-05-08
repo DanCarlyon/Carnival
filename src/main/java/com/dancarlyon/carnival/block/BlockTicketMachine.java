689a85d955dc420c94640328c934fceafecc7e6b
Dan Carlyon <dannycar@hotmail.co.uk>
2016-05-08 11:16:13 +0100

    Added item return to ticket machine

--- src/main/java/com/dancarlyon/carnival/block/BlockTicketMachine.java
+++ src/main/java/com/dancarlyon/carnival/block/BlockTicketMachine.java
@@ -1,7 +1,13 @@
 package com.dancarlyon.carnival.block;
 
+import com.dancarlyon.carnival.init.ModFoodItems;
+import com.dancarlyon.carnival.init.ModItems;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
@@ -13,25 +19,30 @@ public class BlockTicketMachine extends BlockCarnival
     public BlockTicketMachine()
     {
         super();
-        this.setBlockName("swordinstone");
+        this.setBlockName("ticketmachine");
         this.setHardness(50.0F);
         this.setResistance(2000.0F);
-        this.setBlockTextureName("swordinstone");
+        this.setBlockTextureName("ticketmachine");
 
     }
 
-    public boolean onBlockActivated(World world, int p_149727_2_, int p_149727_3_, int p_149727_4_, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
     {
-        if(player.getHeldItem() != null && player.getHeldItem().getItem().getUnlocalizedName().equals("item.carnival:carnival_ticket")) {
-            //player.addChatComponentMessage(new ChatComponentText(player.getDisplayName() + " Cha-Ching"));
-            //world.playSound(1D, 1D, 1D, "carnival:ticket.cha_ching", 1F, 1.0F, false);
-            //.playSoundAtEntity(EntityPlayerObject, "mod_id:hit", 1.0F, 1.0F);
-            world.playSoundAtEntity(player, "carnival:cha_ching", 1, 1);
-            //world.playSoundEffect(, 70, 20, "carnival:cha_ching", 1, 1);
-        } else {
-            player.addChatComponentMessage(new ChatComponentText(player.getDisplayName() + " Maybe I should try something else."));
+        if(!world.isRemote) {
+            if (player.getHeldItem() != null && player.getHeldItem().getItem().getUnlocalizedName().equals("item.carnival:carnival_ticket")) {
+                //player.addChatComponentMessage(new ChatComponentText(player.getDisplayName() + " Cha-Ching"));
+                world.playSoundAtEntity(player, "carnival:cha_ching", 2, 1);
+                spawnAnItem(world, x, y, z);
+            } else {
+                player.addChatComponentMessage(new ChatComponentText(player.getDisplayName() + " Maybe I should try something else."));
+            }
         }
         return false;
+    }
+
+    private void spawnAnItem(World world, int x, int y, int z) {
+        EntityItem ent = new EntityItem(world, x, y, z, new ItemStack(ModItems.carnivalToken));
+        world.spawnEntityInWorld(ent);
     }
 
     @Override
