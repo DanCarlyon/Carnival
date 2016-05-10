f1c23586b3bbfdf1e9afe96b42683f214ca4db72
Dan Carlyon <dannycar@hotmail.co.uk>
2016-05-10 19:00:11 +0100

    Trying to get block rotation working

--- src/main/java/com/dancarlyon/carnival/block/BlockTicketMachine.java
+++ src/main/java/com/dancarlyon/carnival/block/BlockTicketMachine.java
@@ -1,15 +1,15 @@
 package com.dancarlyon.carnival.block;
 
-import com.dancarlyon.carnival.init.ModFoodItems;
 import com.dancarlyon.carnival.init.ModItems;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import com.dancarlyon.carnival.utility.LogHelper;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public class BlockTicketMachine extends BlockCarnival
@@ -30,7 +30,6 @@ public class BlockTicketMachine extends BlockCarnival
     {
         if(!world.isRemote) {
             if (player.getHeldItem() != null && player.getHeldItem().getItem().getUnlocalizedName().equals("item.carnival:carnival_ticket")) {
-                //player.addChatComponentMessage(new ChatComponentText(player.getDisplayName() + " Cha-Ching"));
                 world.playSoundAtEntity(player, "carnival:cha_ching", 2, 1);
                 spawnAnItem(world, x, y, z);
             } else {
@@ -55,6 +54,34 @@ public class BlockTicketMachine extends BlockCarnival
     @Override
     public IIcon getIcon(int side, int meta) {
         return this.icons[side];
+    }
+
+
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack p_149689_6_)
+    {
+        LogHelper.info("x: "+x+" y: "+y+" z: "+z);
+        int l = MathHelper.floor_double((double)(entity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        LogHelper.info(l);
+        if (l == 0)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, 2, 2);
+        }
+
+        if (l == 1)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, 5, 2);
+        }
+
+        if (l == 2)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, 3, 2);
+        }
+
+        if (l == 3)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, 4, 2);
+        }
+
     }
 
 }
